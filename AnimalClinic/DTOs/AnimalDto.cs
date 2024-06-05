a640f3d0ab9902bd6b5590c47107da117ffc7c32
s28222 <s28222@pjwstk.edu.pl>
2024-06-05 13:26:20 +0200

    Add DTO classes to create animal, get animal and update animal

---
+++ AnimalClinic/DTOs/AnimalDto.cs
@@ -0,0 +1,9 @@
+namespace AnimalClinic.DTOs;
+
+public class AnimalDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string AnimalType { get; set; }
+}
---
+++ AnimalClinic/DTOs/UpdateAnimalDto.cs
@@ -0,0 +1,7 @@
+namespace AnimalClinic.DTOs;
+
+public class UpdateAnimalDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
