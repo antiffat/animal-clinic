1451d4eb2665223ba6af309d843967e912ee5b73
s28222 <s28222@pjwstk.edu.pl>
2024-06-13 18:57:56 +0200

    Implement LoginUserDto

---
+++ src/AnimalClinic/DTOs/User/LoginUserDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalClinic.DTOs;
+
+public class LoginUserDto
+{
+    [Required(ErrorMessage = "Username is required.")]
+    public string Username { get; set; }
+    
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; set; }
+}
